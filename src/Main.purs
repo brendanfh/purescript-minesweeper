f79e5a30ec9a846c6cbed1b4fda3e789522f1317
Brendan <brendan.f.hansen@gmail.com>
2015-11-22 04:59:44 +0000

    adding files

---
+++ src/Main.purs
@@ -0,0 +1,10 @@
+module Main where
+
+import Prelude
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
+
+main :: forall e. Eff ( console :: CONSOLE | e ) Unit
+main = do
+  print "Hello sailor!"
+  return unit
